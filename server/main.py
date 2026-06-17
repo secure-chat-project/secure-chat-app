8d8d36e7894b0e3b9588f73eedc85a3acfd32e35
Max Koval <kovalm@wit.edu>
2026-06-17 11:14:20 -0400

    added post register in FastAPI

--- server/main.py
+++ server/main.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+users = {}
+
+class RegisterRequest(BaseModel):
+    username: str
+    public_key: str
 
 @app.get("/")
 def home():
     return {
         "status": "running",
         "project": "Secure Chat App"
+    }
+
+@app.post("/register")
+def register(data: RegisterRequest):
+    if data.username in users:
+        return {
+            "success": False,
+            "message": "Username already exists",
+        }
+
+    users[data.username] = data.public_key
+    return {
+        "success": True,
+        "message": f"User {data.username} created",
     }
